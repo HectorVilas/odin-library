71535cd015cf7e25af1bffe7ada93f5c833a5cff
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-08-20 16:17:17 -0300

    temporal code to test read mark

--- code.js
+++ code.js
@@ -7,3 +7,6 @@ for(let i = 0; i < 15; i++){
   main.appendChild(clone);
 }
 
+const cards = document. querySelectorAll(".card");
+[cards[2],cards[4],cards[5],cards[8],cards[9],cards[12],cards[14]]
+  .forEach(crd => crd.classList.add("read"));
