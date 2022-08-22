8685ea7d4f8c0dc6024b9eb45dd40b6cde93a0b0
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-08-22 17:37:33 -0300

    code to generate cards from array

--- code.js
+++ code.js
@@ -1,12 +1,61 @@
+let myLibrary = [
+  {title: "book 1", author: "author 1", pages: 100, read: false},
+  {title: "book 2", author: "author 2", pages: 200, read: true},
+  {title: "book 3", author: "author 3", pages: 300, read: false},
+  {title: "book 4", author: "author 4", pages: 400, read: false},
+  {title: "book 5", author: "author 5", pages: 500, read: false}
+];
+
 const
   main = document.querySelector("main"),
   card = document.querySelector(".card");
 
-for(let i = 0; i < 15; i++){
-  const clone = card.cloneNode(true);
-  main.appendChild(clone);
-}
 
-const cards = document. querySelectorAll(".card");
-[cards[2],cards[4],cards[5],cards[8],cards[9],cards[12],cards[14]]
-  .forEach(crd => crd.classList.add("read"));
+function Book() {
+    // the constructor...
+};
+
+function addBookToLibrary() {
+  // do stuff here
+};
+
+function placeBooks() {
+  main.innerHTML = "";
+
+  myLibrary.forEach((book, i) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    
+    if(book.read) card.classList.add("read");
+    
+    card.setAttribute("data-index", i);
+
+    const title = document.createElement("p");
+    title.classList.add("title");
+    title.innerText = book.title;
+    card.appendChild(title);
+    
+    const author = document.createElement("p");
+    author.classList.add("author");
+    author.innerText = `by ${book.author}`;
+    card.appendChild(author);
+
+    const pages = document.createElement("p");
+    pages.classList.add("pages");
+    pages.innerText = `${book.pages} pages`;
+    card.appendChild(pages);
+    const edit = document.createElement("button");
+    edit.classList.add("edit");
+    const image = document.createElement("img");
+    image.src = "./media/icons/edit.png";
+    edit.appendChild(image);
+    card.appendChild(edit);
+
+    main.appendChild(card);
+  });
+};
+
+
+
+//run on load
+placeBooks();
